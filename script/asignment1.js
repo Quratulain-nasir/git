cef0449853c6182156779f4f6e90ccd4712f947b
Quratulain-nasir <annienasir203@gmail.com>
2023-04-18 22:57:09 +0500

    asignment1

---
+++ script/asignment1.js
@@ -0,0 +1,132 @@
+// -------Task 01 ------------ 
+
+// var zakat = 0.025;
+// var userInput = +prompt("Write a Number");
+
+// var zakatAmount = zakat * userInput;
+
+// alert("Your Zakat Value is: " +  zakatAmount); 
+
+
+
+
+
+// -------Task 02 ------------ 
+
+// var userinput1=+prompt("Number of members in your family");
+// var userinput2=prompt("wheat,dates");
+
+// var wheat=170
+// var dates=1600
+
+// if (userinput2=='wheat') {
+//    var total = userinput1*wheat
+//    alert(total)}
+
+// if (userinput2=='dates') {
+//    var total = userinput1*dates
+//    alert( total)}
+
+
+
+
+
+// -------Task 03 ------------ 
+
+// var secretnumber= 3
+// var num= +prompt("Guess the secret number")
+
+// if (num >=4) {
+//   alert ("Guess number is high")} 
+
+// if (num <=2) {
+//    alert ("Guess number is low")}
+// if (num==secretnumber) {
+//   alert ("Congratulation.. You successfully Guessed the number")}
+
+
+
+
+
+
+// -------Task 04 ------------ 
+
+// var q=prompt("Enter Your Name")
+
+// var username= "qurat ul ain";
+// var username = (username.charAt(0).toUpperCase()+username.slice(1));
+
+// alert(username);
+
+
+
+
+
+// -------Task 05 ------------ 
+
+// var contactname =[];
+// var contactnumber =[];
+
+// var input1=prompt("Enter Your Name");
+// var input2=prompt("Enter Your Number");
+
+// contactname.push('Name');
+// contactnumber.push('Number');
+
+// console.log(input1+input2);
+
+
+
+
+
+// -------Task 06 ------------  
+
+// var Products =[ "Abaya" , "Hijab-cap" , "Hijajb" , "Naqab" ]
+// alert ( Products)
+// var Products1 = +prompt(  'Enter the position of Product you want')
+// var ProductRemoved = Products.splice(+Products1,1)
+// console.log(ProductRemoved)
+// alert( 'Product Removed:'  + ProductRemoved)
+// alert( 'Remaining Items \n'  +Products)
+// alert( 'Total No Of Remaining Items:'  +Products.length)
+
+
+
+
+
+// -------Task 07 ------------ 
+
+// var nationality = prompt("Enter Your Nationality")
+// if (nationality == "pakistani" || nationality == "Pakistani" || nationality == "Indian" || nationality == "indian") {
+//   var gender = prompt("Enter Gender")
+//   var age = +prompt("Enter Age")
+//   if (gender == "male" || gender == "Male") {
+//     if (age >= 18) { alert("You Are Eligible For Vote") }
+//     else { alert("You Are Not Eligible For Vote") }
+//   }
+//   if (gender == "female" || gender == "Female") {
+//     if (age >= 18) {
+//       var status = prompt("Are You Married?")
+//       if (Status == "Married" || Status == "married") { alert("You Are Eligible For Vote") }
+//       else { alert("You Are Not Eligible For Vote") }
+//     } else { alert("You Are Not Eligible For Vote") }
+//   }
+// }
+// else { alert("You Are Not Eligible For Vote") }
+
+
+
+
+
+
+// -------Task 08 ------------ 
+
+// var WorldCupSquad=['Babar Azam','Shadab Khan', 'Asif Ali', 'Fakhar Zaman',' Haider Ali', 'Haris Rauf', 'Iftikhar Ahmed', 'Khushdil Shah', 'Mohammad Hasnain',' Mohammad Nawaz', 'Mohammad Rizwan', 'Mohammad Wasim',' Naseem Shah','Shaheen Shah Afridi',' Shan Masood']
+// alert  (WorldCupSquad)
+// var  Finalteamplayers= ['Babar Azam','Shadab Khan', 'Asif Ali', 'Fakhar Zaman',' Haider Ali', 'Haris Rauf', 'Iftikhar Ahmed', 'Khushdil Shah', 'Mohammad Hasnain',' Mohammad Nawaz', 'Mohammad Rizwan']
+// alert (Finalteamplayers)
+
+
+
+
+
